eb1c559dcb277f48f717e584aa5753e1adff4c4a
peng <peng.zi@husky.neu.edu>
2019-01-30 03:01:15 +0000

    modified deloy scripts

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :memory, MemoryWeb.Endpoint,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "http://hw04.zy-peng.com", port: 2222],
+  url: [host: "hw04.zy-peng.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
